863a0a3d5e0bb767de6d9087e80e3bcc1e8ec993
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-08-27 19:21:50 +0300

    Add like counter method to like module

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :author_id, class_name: 'User'
   belongs_to :post
 
-  def three_most_recent_posts
-    posts.order(created_at: :desc).limit(3)
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
