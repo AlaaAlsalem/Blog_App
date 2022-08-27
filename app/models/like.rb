d4cf377dd190f442b0ebb275e81e0d9e51e8d5dc
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-08-27 18:33:16 +0300

    Fix linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    def three_most_recent_posts
-        posts.order(created_at: :desc).limit(3)
-      end
-    
-
-  
+  def three_most_recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
-  
+end
