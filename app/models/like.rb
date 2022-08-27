9877f6094e121e936c88115bd4de6514c6372e1c
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-08-27 18:00:49 +0300

    Add A method that returns the 3 most recent posts for a given user

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,12 @@
 class Like < ApplicationRecord
     belongs_to :user
     belongs_to :post
+
+    def three_most_recent_posts
+        posts.order(created_at: :desc).limit(3)
+      end
+    
+
   
   end
   
