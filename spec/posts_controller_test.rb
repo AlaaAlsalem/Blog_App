4b97a482b3e26d478ecac0788d04b3eca9010bc4
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-09-03 21:02:11 +0300

    Fix linter errors

--- spec/posts_controller_test.rb
+++ spec/posts_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     get posts_index_url
     assert_response :success
--- spec/users_controller_test.rb
+++ spec/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
- RSpec.describe "Posts", type: :request do
+RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     get users_index_url
     assert_response :success
